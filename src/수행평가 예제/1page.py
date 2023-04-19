72d3dcf263df70728c427f6406f242b56f46c788
woong <koohw0109@gmail.com>
2023-04-19 23:59:49 +0900

    python test prac

---
+++ src/수행평가 예제/1page.py
@@ -0,0 +1,59 @@
+#1
+# print('I said "Hello" to you')
+# print("Let's go")
+
+#2
+# w = int(input("삼각형의 밑변 길이 : "))
+# h = int(input("삼각형의 높이 : "))
+# area = w * h / 2
+# print("삼각형의 면적: ", area)
+
+#3
+# h = int(input("현재 시 : "))
+# m = int(input("현재 분 : "))
+# m = m + 20
+# h = (h + m // 60) % 24
+# m = m % 60
+# print("20분 뒤에는 %d시 %d분 입니다." %(h, m))
+
+#4
+# num = int(input("숫자 입력 : "))
+# if num < 0:
+#     num = -1 * num
+# print(num)
+
+#5
+# id = input("아이디를 입력하세요 : ")
+# level = int(input("회원레벨을 입력하세요 : "))
+
+# if(id == 'admin' or level == 1):
+#     print("관리자입니다.")
+# else:
+#     print("관리자가 아닙니다.")
+
+#6
+# a = int(input("첫 번째 수 : "))
+# b = int(input("두 번째 수 : "))
+# c = int(input("세 번째 수 : "))
+
+# max = a
+# if max < b:
+#     max = b
+
+# if max < c:
+#     max = c
+
+# print("가장 큰 수는 ", max, "입니다.")
+
+#7
+# age = int(input("나이 입력: "))
+# if age <= 7:
+#     print("어린이")
+# elif age <= 13:
+#     print("초등학생")
+# elif age <= 16:
+#     print("중학생")
+# elif age <= 19:
+#     print("고등학생")
+# else:
+#     print("성인")
---
+++ src/수행평가 예제/2page.py
@@ -0,0 +1,5 @@
+num = int(input("in"))
+
+while num != 0:
+    print(num % 10, end="")
+    num = num // 10
