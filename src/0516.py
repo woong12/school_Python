aebbcf7dfa7a413ddd34a6d2df9b862a16b6ec9b
woong <koohw0109@gmail.com>
2023-05-30 10:31:51 +0900

    0516

--- src/0516.py
+++ src/0516.py
@@ -363,16 +363,47 @@ print("총점은 %d이고 평균은 %.1f입니다." % (total, average))
 
 # 34
 """
-
-"""
-
 numbers = [7, 9, 15, 18, 30, -3, 7, 12, -16, -12]
 
 sum = 0
 i = 0
-print("짝수 번째 요소 : ", end="")
+print("짝수 번째 요소 : ", end = " ")
 
-while i < len(numbers):
-    if numbers[i] % 2 == 0:
-        sum += numbers
-print(sum)
+while i< len(numbers[1::2]):
+  print(numbers[2 * i + 1], end = " ")
+  sum += numbers[2 * i + 1] 
+  i += 1
+print("\n합계 : ", sum)
+"""
+
+# 35
+"""
+s = [64, 89, 100, 85, 77, 58, 79, 67, 96, 87, 87, 36, 82, 98, 84, 76, 63, 69, 53, 22]
+
+soo = 0
+woo = 0
+mi = 0
+yang = 0
+ga = 0
+
+i = 0
+
+while i < len(s) :
+  if s[i] >= 90:
+    soo += 1
+  elif s[i] >= 80:
+    woo += 1
+  elif s[i] >= 70:
+    mi += 1
+  elif s[i] >= 60:
+    yang += 1
+  else:
+    ga += 1
+  i+= 1
+
+print("수: %d명" %soo)
+print("우: %d명" %woo)
+print("미: %d명" %mi)
+print("양: %d명" %yang)
+print("가: %d명" %ga)
+"""
